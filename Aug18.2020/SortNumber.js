d0e9d70c8ad8d383cf0df2d6a45a309cc6909d94
estherkimyunjung <estherkimyunjung@gmail.com>
2020-08-18 11:15:03 -0500

    sort number with es6 javascript

--- Aug18.2020/SortNumber.js
+++ Aug18.2020/SortNumber.js
@@ -2,6 +2,8 @@
 //   return(nums===null ? [] : nums.sort(function(a, b){return a - b}))
 // }
 
-function solution(nums){
-  return nums===null ? [] : nums.sort((a, b) => a - b)
-}
+// function solution(nums){
+//   return nums !== null ? nums.sort((a, b) => a - b) : [];
+// }
+
+const solution = nums => (nums || []).slice().sort((a, b) => a - b);
