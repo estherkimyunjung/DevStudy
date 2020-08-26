5424b8143a5f7fa77b69d4084ef8e5508d53b962
estherkimyunjung <estherkimyunjung@gmail.com>
2020-08-25 22:43:03 -0500

    anagrams practice

---
+++ 082420_problemApproach.js
@@ -0,0 +1,18 @@
+//Algorithm a process or set of steps to accomplish a certain task
+//1. devise a plan for the solving problems
+//2. Master common problem solving patterns
+
+// PROBLEMS SOLVING
+// 1. Understand the problems 
+//  - can I restate the problem in my own word?
+//  - what are the input that go into the problem?
+//  - what are the output that should come from the solution to the problem?
+//  - can the outputs be determined from the inputs? In other words, do I have enough information to the slove problem?
+//  - How should I label the important pieces of data that are a part of the problem?
+// 2. Explore concrete examples (user stories and unit tests)
+//  - Coming up with examples can help you understand the problem better
+//  - Example also provide sanity checks that your eventual solution works how it should
+// 3. Break it down
+//  - 
+// 4. Solve/Simplify
+// 5. Look back and refactor
