ad8c637586334524b27a34ad885a15bbe74ea18a
estherkimyunjung <estherkimyunjung@gmail.com>
2020-09-11 15:16:00 -0500

    solved capitalize

--- JavaScript-Algorithms_DataStructures/083120_Capitalization.js
+++ JavaScript-Algorithms_DataStructures/083120_Capitalization.js
@@ -1,3 +1,22 @@
+// function capitalize(str) {
+//   return str[0].toUpperCase() + str.slice(1).toLowerCase()
+// }
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
 function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1).toLowerCase()
+  const words = [];
+
+  for(let word of str.split(' ')) {
+    word.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return word.join(' ')
 }
+
