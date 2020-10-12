d29c800e3317f2f03d84799227ffc63abd67b499
estherkimyunjung <estherkimyunjung@gmail.com>
2020-10-12 15:23:00 -0500

    change folder name

--- JavaScript-Algorithms_DataStructures/081120_diagonalDifference.js
+++ JavaScript-Algorithms_DataStructures/081120_diagonalDifference.js
@@ -12,7 +12,7 @@ function diagonalDifference(arr) {
   for (let i = 0; i < n; i++) {
       sum += arr[i][i] - arr[i][n-1-i];
   }
-  return(sum >= 0 ? sum : sum * -1);
+  console.log(sum >= 0 ? sum : sum * -1);
 }
 
 diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]])
