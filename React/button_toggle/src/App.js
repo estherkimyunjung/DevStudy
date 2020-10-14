a1f31ab38f18fe2331eb969173cf43c869e1ae46
estherkimyunjung <estherkimyunjung@gmail.com>
2020-10-14 14:08:54 -0500

    react toggle button event

--- React/button_toggle/src/App.js
+++ React/button_toggle/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = { 
+    isClick: ''
+  }
 
+  clickHandler = (isClick) => {
+    this.setState ({
+      isClick: isClick
+    })
+  }
+
+  render() { 
+    return ( 
+      <div>
+        <button
+        style={{fontWeight: this.state.isClick === 'A' ? 'Bold' : ''}}
+        onClick={() => {this.clickHandler("A")}}
+        >
+          Button A
+        </button>
+        <button
+        style={{fontWeight: this.state.isClick === 'B' ? 'Bold' : ''}}
+        onClick={() => {this.clickHandler("B")}}
+        >
+          Button B
+        </button>
+      </div>
+     );
+  }
+}
+ 
 export default App;
