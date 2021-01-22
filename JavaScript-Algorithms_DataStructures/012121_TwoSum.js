40ff5b372beffd086c60e2347ec50c82c974e395
estherkimyunjung <estherkimyunjung@gmail.com>
2021-01-21 18:32:55 -0600

    leecode practice

---
+++ JavaScript-Algorithms_DataStructures/012121_TwoSum.js
@@ -0,0 +1,39 @@
+// function twoSum(nums, target) {
+//   // console.log('nums :', nums);
+//   // console.log('target :', target);
+//   if(nums.length >=2){
+//     for (let i = 0 ; i <= nums.length; i++){
+//       // const first = nums[i]
+//       // const second = nums[i+1]
+//       // if (first + second === target){
+//       // console.log(`[${i},${i+1}]`);
+//       // } 
+//       for(let j = i+1; j <= nums.length; j++){
+//         const first = nums[i]
+//         const second = nums[j]
+//         if(first + second === target){
+//           return console.log(`[${i},${j}]`)
+//           // return [i,j]
+//         } 
+//       }
+//     }
+//   } else {
+//     return console.log('undefined')
+//   }
+// }
+
+// // twoSum([2,7,11,15], 9)
+// // twoSum([3,2,4], 6)
+
+const twoSum = (nums, target) => {
+  const result = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in result) {
+      return [result[target - nums[i]], i];
+    } else {
+      result[nums[i]] = i;
+    }
+  }
+}
+
+twoSum([2,5,5,11], 10)
---
+++ JavaScript-Algorithms_DataStructures/012121_validParentheses.js
@@ -0,0 +1,43 @@
+
+var isValid = function(s) {
+  // const newArray = s.split('')
+
+  // console.log('s:',s)
+  // if (newArray.length % 2 === 0){
+  //   for(let i = 0; i<= newArray.length; i++){
+  //     for(let j = i+1; j <= newArray.length; j++){
+  //       if((newArray[i]==='(' && newArray[j] ===')') || (newArray[i]==='{' && newArray[j] ==='}') || (newArray[i]==='[' && newArray[j] ===']')){
+  //         return console.log('true')
+  //       } else {
+  //         return console.log ('false')
+  //       }
+  //     }
+  //   }
+  // } else {
+  //   return console.log('false')
+  // }
+
+  const obj = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  }
+
+  const stack = [];
+
+  for (const paran of s) {
+    if (obj.hasOwnProperty(paran)) {
+      stack.push(paran)
+    } else {
+      const closeParan = stack.pop();
+      if (paran !== obj[closeParan]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+
+};
+
+isValid("{(}")
