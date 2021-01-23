50bbb777b13752523c390abe3abe8b0f57296b71
estherkimyunjung <estherkimyunjung@gmail.com>
2021-01-23 01:49:48 -0600

    add a comment on solution

--- JavaScript-Algorithms_DataStructures/012221_climbStair.js
+++ JavaScript-Algorithms_DataStructures/012221_climbStair.js
@@ -12,25 +12,25 @@ const climbStairs = (n) => {
 //     obj[n] = n;
 //     console.log(n)    
 //       return n;
-//   } else {
-//     obj = {1:1, 2:2};      
-//     for(let i = 3; i <= n+1; i++){
-//       let result = obj[i-1] + obj[i-2]
-//       obj[i] = result
-//     }
-//     console.log(obj[n])      
-//     return obj[n]
+//   } else {     --------- I created object for if statement so if statement is duplicate case
+    let obj = {1:1, 2:2};      
+    for(let i = 3; i <= n+1; i++){
+      let result = obj[i-1] + obj[i-2]
+      obj[i] = result
+    }
+    console.log(obj[n])      
+    return obj[n]
 //   } 
 
 
 // 2. use array
 
-  let result = [1, 1];
-  for(let i = 2; i <=n; i++) {
-  result[i] = result[i-1] + result[i-2];
-  }
-  console.log(result[n])
-  return result[n]
+  // let result = [1, 1];
+  // for(let i = 2; i <=n; i++) {
+  // result[i] = result[i-1] + result[i-2];
+  // }
+  // console.log(result[n])
+  // return result[n]
 
 }
 
