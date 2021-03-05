3c7930931c3c1d245b138b201fdb8ed5467db8f9
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-04 19:42:33 -0600

    sliding window pattern refactor

--- JavaScript-Algorithms_DataStructures/03_2021/maxSubarraySum.js
+++ JavaScript-Algorithms_DataStructures/03_2021/maxSubarraySum.js
@@ -1,18 +1,42 @@
+// sliding window pattern by loop  O(n^2)
+// const maxSubarraySum = (arr, num) => {
+//   if (num > arr.length) {
+//     return console.log(null);
+//   }
+//   let max = -Infinity; // if there is - num
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > max) {
+//       max = temp;
+//     }
+//   }
+//   return console.log(max);
+// };
+
+//Refactor O(n)
+
 const maxSubarraySum = (arr, num) => {
-  if (num > arr.length) {
+  if (arr.length < num) {
     return console.log(null);
   }
-  let max = -Infinity; // if there is - num
-  for (let i = 0; i < arr.length - num + 1; i++) {
-    temp = 0;
-    for (let j = 0; j < num; j++) {
-      temp += arr[i + j];
-    }
-    if (temp > max) {
-      max = temp;
-    }
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
-  return console.log(max);
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return console.log(maxSum);
 };
 
 maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
