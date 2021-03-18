42f15fa227cbe7a30fa8f75a8229e4cbeab04fe1
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-17 22:46:45 -0500

    practice different way

--- JavaScript-Algorithms_DataStructures/03_2021/fileSearch.js
+++ JavaScript-Algorithms_DataStructures/03_2021/fileSearch.js
@@ -23,86 +23,86 @@
 // // create a files array that holds all files when found
 // // create a fileMap that uses the content of the files as the key and the names as the value in an array. (we'll push this array into our answer array later to gain our nested array answer)
 // let isDuplicate = (str) => {
-//   let array = [];
-//   let fileMap = {};
-//   let answer = [];
-//   let currentDirectory = str;
-//   let recursive = (strng) => {
-//     let files = getFileContent(strng);
-//     for (let i = 0; i < files.length; i++) {
-//       let file = file[i];
-//       if (isDirectory(file)) {
-//         currentDirectory = currentDirectory + file;
-//         recursive(file);
-//       } else {
-//         array.push(currentDirectory + "/" + file);
+// //   let array = [];
+// //   let fileMap = {};
+// //   let answer = [];
+// //   let currentDirectory = str;
+// //   let recursive = (strng) => {
+// //     let files = getFileContent(strng);
+// //     for (let i = 0; i < files.length; i++) {
+// //       let file = file[i];
+// //       if (isDirectory(file)) {
+// //         currentDirectory = currentDirectory + file;
+// //         recursive(file);
+// //       } else {
+// //         array.push(currentDirectory + "/" + file);
+// //       }
+// //     }
+// //   };
+// //   recursive(str);
+// //   for (let i = 0; i < array.length; i++) {
+// //     let file = array[i];
+// //     fileMap[file] ? fileMap[i].push(file) : (fileMap[i] = [file]);
+// //   }
+// //   for (let key in fileMap) {
+// //     answer.push(fileMap[key]);
+// //   }
+// //   return answer.filter((array) => array.length > 1);
+// // };
+
+// // give a string with filepath write a function that check all content is duplicate and return the duplicate contents file path.
+
+// // Is file name without content? - No worry about the case
+// // Is file is excited? - always file in the path.
+// // when return the order in not matter.
+
+// // create function findDuplicate
+
+// function findDuplicate(arr) {
+//   let fileMap = new Map();
+//   // value will be path (path and join file name with /
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let filePaths = arr[i].split(" ");
+//     let rootPath = filePaths[0];
+//     console.log("-----------------");
+//     console.log(rootPath);
+//     console.log("filePaths", filePaths);
+//     console.log("-----------------");
+
+//     let file = arr[i].split("");
+
+//     console.log(file);
+//     // Hashmap with key => content
+//     let openParIdx = 0;
+//     let closeParIdx = 0;
+
+//     for (let j = 0; j < file.length; j++) {
+//       const fileChar = file[j];
+//       let tempKey = "";
+
+//       if (fileChar === "(") {
+//         openParIdx = j;
+//       } else if (fileChar === ")") {
+//         closeParIdx = j;
+//         console.log("openParIdx", openParIdx);
+//         console.log("closeParIdx", closeParIdx);
+
+//         const tempKey = file.slice(openParIdx + 1, closeParIdx).join("");
+
+//         let fileName = arr[i].split(" ").join("/").substr(0, openParIdx);
+//         console.log(fileName);
+
+//         fileMap.set(tempKey, fileName);
+//         console.log("FM", fileMap);
 //       }
 //     }
-//   };
-//   recursive(str);
-//   for (let i = 0; i < array.length; i++) {
-//     let file = array[i];
-//     fileMap[file] ? fileMap[i].push(file) : (fileMap[i] = [file]);
 //   }
-//   for (let key in fileMap) {
-//     answer.push(fileMap[key]);
-//   }
-//   return answer.filter((array) => array.length > 1);
-// };
 
-// give a string with filepath write a function that check all content is duplicate and return the duplicate contents file path.
+//   // loop through arr and split with space to create root path and file name and content
 
-// Is file name without content? - No worry about the case
-// Is file is excited? - always file in the path.
-// when return the order in not matter.
-
-// create function findDuplicate
-
-function findDuplicate(arr) {
-  let fileMap = new Map();
-  // value will be path (path and join file name with /
-
-  for (let i = 0; i < arr.length; i++) {
-    let filePaths = arr[i].split(" ");
-    let rootPath = filePaths[0];
-    console.log("-----------------");
-    console.log(rootPath);
-    console.log("filePaths", filePaths);
-    console.log("-----------------");
-
-    let file = arr[i].split("");
-
-    console.log(file);
-    // Hashmap with key => content
-    let openParIdx = 0;
-    let closeParIdx = 0;
-
-    for (let j = 0; j < file.length; j++) {
-      const fileChar = file[j];
-      let tempKey = "";
-
-      if (fileChar === "(") {
-        openParIdx = j;
-      } else if (fileChar === ")") {
-        closeParIdx = j;
-        console.log("openParIdx", openParIdx);
-        console.log("closeParIdx", closeParIdx);
-
-        const tempKey = file.slice(openParIdx + 1, closeParIdx).join("");
-
-        let fileName = arr[i].split(" ").join("/").substr(0, openParIdx);
-        console.log(fileName);
-
-        fileMap.set(tempKey, fileName);
-        console.log("FM", fileMap);
-      }
-    }
-  }
-
-  // loop through arr and split with space to create root path and file name and content
-
-  //return value will be full path with file name by content
-}
+//   //return value will be full path with file name by content
+// }
 
 // var findDuplicate = function (paths) {
 //   let group = {};
@@ -130,9 +130,60 @@ function findDuplicate(arr) {
 //   return console.log(Object.values(map).filter((dups) => dups.length > 1));
 // };
 
+// give a string with filepath write a function that check all content is duplicate and return the duplicate contents file path.
+
+// Is file name without content? - No worry about the case
+// Is file name can be duplicate with same path?
+// Is file always existing in the directory.
+// when return the order in not matter.
+
+// create function findDuplicate
+
+const findDuplicate = (paths) => {
+  //parse the paths
+  //extract file name and contents
+  //create out array
+  const conentMap = new Map();
+  let results = [];
+
+  for (let path of paths) {
+    const [dir, ...files] = path.split(" ");
+    console.log("path", path);
+
+    if (files.length > 0) {
+      files.forEach((file) => {
+        const conentStart = file.indexOf("(");
+
+        //parse content
+        const content = file.substring(conentStart + 1, file.length - 1);
+
+        //parse filename
+        const fileName = file.substring(0, conentStart);
+
+        const fullPath = dir + "/" + fileName;
+
+        if (conentMap.has(content)) {
+          conentMap.set(content, [...conentMap.get(content), fullPath]);
+        } else {
+          conentMap.set(content, [fullPath]);
+        }
+      });
+    }
+  }
+
+  for (let [key, val] of conentMap) {
+    if (val.length > 1) {
+      results.push(val);
+    }
+  }
+
+  return console.log(results);
+};
+
 findDuplicate([
   "root/a 1.txt(abcd) 2.txt(efgh)",
   "root/c 3.txt(abcd)",
   "root/c/d 4.txt(efgh)",
   "root 4.txt(efgh)",
+  "root/e ",
 ]);
