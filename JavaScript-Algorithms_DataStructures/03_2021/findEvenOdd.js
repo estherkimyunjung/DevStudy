e6060a7f087483507773192ed7f3df9ffd43f3d6
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-26 17:14:29 -0500

    find even odd number

---
+++ JavaScript-Algorithms_DataStructures/03_2021/findEvenOdd.js
@@ -0,0 +1,7 @@
+//write a function return string even with even number or odd with odd numbers
+
+//
+function findEventOdd(number) {
+  console.log(number % 2 === 0 ? "even" : "add");
+}
+isEvent(3);
